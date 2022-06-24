12801ef2c0879d83f7b50d1bac49bd079d1375f2
Bitcorse <104611442+Bitcorse@users.noreply.github.com>
2022-06-24 20:46:22 +0800

    Add files via upload

---
+++ precache-manifest.034c8c30f4d97448f7623ed6ae7fd456.js
@@ -0,0 +1,262 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3355c391ed0a5cfbfb2b15754fcc5b67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2113d36ddf5399dacc96",
+    "url": "/static/css/main.1e2222cb.chunk.css"
+  },
+  {
+    "revision": "0fa8c70f425ab630a9b6",
+    "url": "/static/js/0.d800fabb.chunk.js"
+  },
+  {
+    "revision": "13504c073c718c9be608",
+    "url": "/static/js/1.0f073fa0.chunk.js"
+  },
+  {
+    "revision": "8ed26fa3da5f9c83fd94",
+    "url": "/static/js/10.2ed92e39.chunk.js"
+  },
+  {
+    "revision": "32094cb8886182d9a20e",
+    "url": "/static/js/11.58ce877b.chunk.js"
+  },
+  {
+    "revision": "9f66570a99199c67399e",
+    "url": "/static/js/12.0d731ae5.chunk.js"
+  },
+  {
+    "revision": "41fd537451f47aa2edea",
+    "url": "/static/js/13.396eba05.chunk.js"
+  },
+  {
+    "revision": "7e92eda1e20287b16ba5",
+    "url": "/static/js/14.26108df4.chunk.js"
+  },
+  {
+    "revision": "cf1edb21747accdcfe60",
+    "url": "/static/js/15.64b9166f.chunk.js"
+  },
+  {
+    "revision": "9797b036564e507b82ed",
+    "url": "/static/js/16.aff9463a.chunk.js"
+  },
+  {
+    "revision": "205f87f04c4fa7aed33d",
+    "url": "/static/js/17.2bb82a16.chunk.js"
+  },
+  {
+    "revision": "79ead5b83344a90faea8",
+    "url": "/static/js/18.8ec6fd91.chunk.js"
+  },
+  {
+    "revision": "9c9b22a46db64c57c5af",
+    "url": "/static/js/19.9b448e42.chunk.js"
+  },
+  {
+    "revision": "a7bc6b727282624fdf07",
+    "url": "/static/js/2.8ce82412.chunk.js"
+  },
+  {
+    "revision": "19a943b4b17e5e1fc3d6",
+    "url": "/static/js/20.11dd2c58.chunk.js"
+  },
+  {
+    "revision": "1c1213c913d8a0d4068bc13c83f7e53c",
+    "url": "/static/js/20.11dd2c58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c173c29a6ee4e346c9d",
+    "url": "/static/js/21.bd032e8a.chunk.js"
+  },
+  {
+    "revision": "b9d54b9c1babad4e1de7",
+    "url": "/static/js/22.e40b9510.chunk.js"
+  },
+  {
+    "revision": "23ff3b94a56a96873421",
+    "url": "/static/js/23.1e0cde7d.chunk.js"
+  },
+  {
+    "revision": "f407b818be51a92f59ad",
+    "url": "/static/js/24.c81b0606.chunk.js"
+  },
+  {
+    "revision": "b6034b9f71be5be49ccb",
+    "url": "/static/js/25.93c08cbc.chunk.js"
+  },
+  {
+    "revision": "f93c0b1c07e5f6fe8529",
+    "url": "/static/js/26.2e3a65c2.chunk.js"
+  },
+  {
+    "revision": "c17e99bbe594cd06c52b",
+    "url": "/static/js/27.a9262212.chunk.js"
+  },
+  {
+    "revision": "f7fed77c3aba85401376",
+    "url": "/static/js/28.40a37082.chunk.js"
+  },
+  {
+    "revision": "d7b669443db3e4b5bca2",
+    "url": "/static/js/29.404fc7f1.chunk.js"
+  },
+  {
+    "revision": "266eb282f84655c60d8b",
+    "url": "/static/js/3.fa15f495.chunk.js"
+  },
+  {
+    "revision": "f8f764174154403196a1",
+    "url": "/static/js/30.fe790a3e.chunk.js"
+  },
+  {
+    "revision": "531cf649113c00c7f31b",
+    "url": "/static/js/31.d87e0975.chunk.js"
+  },
+  {
+    "revision": "153364367fdcec774593",
+    "url": "/static/js/4.f5e4d5ff.chunk.js"
+  },
+  {
+    "revision": "8ed877d5916ae5c314f8",
+    "url": "/static/js/7.ade13436.chunk.js"
+  },
+  {
+    "revision": "a3263fd2ea11ca7104c2f8fbbd80eef2",
+    "url": "/static/js/7.ade13436.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa19defe88e38a01ee13",
+    "url": "/static/js/8.ba81d163.chunk.js"
+  },
+  {
+    "revision": "bf9f179ebcbf53b797af01d432e4ced8",
+    "url": "/static/js/8.ba81d163.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bce2c87eb609490162f",
+    "url": "/static/js/9.6a8996ab.chunk.js"
+  },
+  {
+    "revision": "086ac0f260c0d88cf399717695349565",
+    "url": "/static/js/9.6a8996ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2113d36ddf5399dacc96",
+    "url": "/static/js/main.b32a329e.chunk.js"
+  },
+  {
+    "revision": "cc9fccfbe41060190c6a",
+    "url": "/static/js/runtime-main.9aad0a58.js"
+  },
+  {
+    "revision": "bcc5271f9dea2e11df6ec3772b2e55d1",
+    "url": "/static/media/CAKE.bcc5271f.png"
+  },
+  {
+    "revision": "c76b33ca42c5730ab77f3341ce9764a7",
+    "url": "/static/media/USDC.c76b33ca.png"
+  },
+  {
+    "revision": "576ce3f3a7d53537fdc54640c3dc51dd",
+    "url": "/static/media/background.576ce3f3.png"
+  },
+  {
+    "revision": "7024ee4355669e3d07951220b1717d93",
+    "url": "/static/media/bbond.7024ee43.svg"
+  },
+  {
+    "revision": "e31bd8e06586eb7f5be62ced8df8cd90",
+    "url": "/static/media/bbond_25.e31bd8e0.svg"
+  },
+  {
+    "revision": "576ce3f3a7d53537fdc54640c3dc51dd",
+    "url": "/static/media/boardroom.576ce3f3.png"
+  },
+  {
+    "revision": "369dd5011008c2b221e6ff39832c9a41",
+    "url": "/static/media/bshare.369dd501.svg"
+  },
+  {
+    "revision": "fa595f70f298eb474575aaef410adad6",
+    "url": "/static/media/bshare_25.fa595f70.svg"
+  },
+  {
+    "revision": "badf6ce30bfe1a85db8de7c191bd7f13",
+    "url": "/static/media/bshare_usdc.badf6ce3.png"
+  },
+  {
+    "revision": "c2731784d1d5cccfeef2ee00caffd53f",
+    "url": "/static/media/bshare_wbnb.c2731784.png"
+  },
+  {
+    "revision": "e0d6284322109f0cacfbbf714b81f5a1",
+    "url": "/static/media/bstable.e0d62843.svg"
+  },
+  {
+    "revision": "1aab0ee1b4f5b58bc237ef0100433a41",
+    "url": "/static/media/bstable_25.1aab0ee1.svg"
+  },
+  {
+    "revision": "b109caa6ec68e7733e489d2990afe92d",
+    "url": "/static/media/bstable_cake.b109caa6.png"
+  },
+  {
+    "revision": "a8a91ad8ee2653ca612e3d38391e097d",
+    "url": "/static/media/bstable_usdc.a8a91ad8.png"
+  },
+  {
+    "revision": "14bccab31d06ef0990ba9522d6a03e7d",
+    "url": "/static/media/bstable_wbnb.14bccab3.png"
+  },
+  {
+    "revision": "3765bf16134347a88f03141d2b07b18d",
+    "url": "/static/media/coinbase_logo.3765bf16.jpeg"
+  },
+  {
+    "revision": "571b30afc5eb40a9cb46703fe9068b4a",
+    "url": "/static/media/discord.571b30af.svg"
+  },
+  {
+    "revision": "576ce3f3a7d53537fdc54640c3dc51dd",
+    "url": "/static/media/farm.576ce3f3.png"
+  },
+  {
+    "revision": "65962ad862aeb907f3b713feadd60e74",
+    "url": "/static/media/github.65962ad8.svg"
+  },
+  {
+    "revision": "576ce3f3a7d53537fdc54640c3dc51dd",
+    "url": "/static/media/home.576ce3f3.png"
+  },
+  {
+    "revision": "c06f3a3e804ebc7343949fdca3fdd7f8",
+    "url": "/static/media/metamask-fox.c06f3a3e.svg"
+  },
+  {
+    "revision": "143aacd88f4af8471910912869a16f5a",
+    "url": "/static/media/telegram.143aacd8.svg"
+  },
+  {
+    "revision": "c2de845a52ed7babfcb656ac76f00b06",
+    "url": "/static/media/telegram_small.c2de845a.svg"
+  },
+  {
+    "revision": "79c654677ca6c4d7ca799ab3b5b79124",
+    "url": "/static/media/twitter.79c65467.svg"
+  },
+  {
+    "revision": "f100f1558c0f42eaedba6615082cf19f",
+    "url": "/static/media/usdt.f100f155.png"
+  },
+  {
+    "revision": "37f2bc6ecee2eb29b3cf6799ce58bf58",
+    "url": "/static/media/wallet-connect.37f2bc6e.svg"
+  },
+  {
+    "revision": "ac01ed52046be1bd9905cf89bb07be70",
+    "url": "/static/media/youtube.ac01ed52.svg"
+  }
+]);
